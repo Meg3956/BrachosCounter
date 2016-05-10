50f9a068a178de685ea3c2f53170d27aff50152b
developermg <meiracgoldberg@gmail.com>
2016-05-10 19:46:38 -0400

    Adding

--- app/src/main/java/com/example/shaina/brachoscounter/DaveningActivity.java
+++ app/src/main/java/com/example/shaina/brachoscounter/DaveningActivity.java
@@ -1,13 +1,16 @@
 package com.example.shaina.brachoscounter;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DaveningActivity extends AppCompatActivity {
+public class DaveningActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_davening);
+        //pass options when create DaveningAdapter
     }
 }
