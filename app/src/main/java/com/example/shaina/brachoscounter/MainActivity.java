3ab845d9bd19f8c9fdd8013523d3563291a9c6d4
developermg <meiracgoldberg@gmail.com>
2016-05-17 18:13:10 -0400

    Adding

--- app/src/main/java/com/example/shaina/brachoscounter/MainActivity.java
+++ app/src/main/java/com/example/shaina/brachoscounter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.shaina.brachoscounter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
