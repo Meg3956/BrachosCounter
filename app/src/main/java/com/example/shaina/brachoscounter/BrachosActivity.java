f7b5f4f17df58af9f5091341ff82ff58d311e21e
Meg <shaina.hirschman@gmail.com>
2016-05-18 22:27:23 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/shaina/brachoscounter/BrachosActivity.java
+++ app/src/main/java/com/example/shaina/brachoscounter/BrachosActivity.java
@@ -1,18 +1,16 @@
 package com.example.shaina.brachoscounter;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 /**
  * Created by Shaina on 5/17/2016.
  */
-public abstract class BrachosActivity extends Activity {
+public abstract class BrachosActivity extends ListActivity {
 
     ListView lview;
     //BirchosHaneheninAdapter listAdapter;
@@ -23,16 +21,15 @@ public abstract class BrachosActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hanehenin);
-        lview = (ListView) findViewById(R.id.listView);
+        //setContentView(R.layout.activity_hanehenin);
+        //lview = (ListView) findViewById(R.id.listView);
         brachos = createArray();
-        ArrayAdapter adapter = new ArrayAdapter(this,
-                R.layout.row_layout_listview, R.id.brachaOption, brachos);
+        BrachosAdapter adapter = new BrachosAdapter(this, brachos);
 
         // Assign adapter to ListView
-        lview.setAdapter(adapter);
+        setListAdapter(adapter);
 
-        lview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+       /* lview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1,
@@ -46,14 +43,20 @@ public abstract class BrachosActivity extends Activity {
                         itemValue + ": ", Toast.LENGTH_LONG)
                         .show();
             }
-        });
+        });*/
     }
+
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
+
 
     //What is this for?
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-     //   getMenuInflater().inflate(R.menu.main, menu);
+        //   getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
