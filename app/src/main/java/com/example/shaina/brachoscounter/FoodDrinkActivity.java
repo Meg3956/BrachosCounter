fe902a1b2b7816b3df4b1416244dbf185d34f7fb
Meg <shaina.hirschman@gmail.com>
2016-05-18 23:05:57 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml

---
+++ app/src/main/java/com/example/shaina/brachoscounter/FoodDrinkActivity.java
@@ -0,0 +1,16 @@
+package com.example.shaina.brachoscounter;
+
+/**
+ * Created by Shaina on 5/18/2016.
+ */
+public class FoodDrinkActivity extends BrachosActivity {
+
+    @Override
+    String[] createArray() {
+
+        String[] foodBrachos = {"Hamotzi", "Mezonos", "Hagafen", "Haetz", "Ha'adama", "Shehakol",
+                "Birkas Hamazon", "Al Hamichya", "Borei Nefashos"};
+
+        return foodBrachos;
+    }
+}
