1daaa9ac649d6d58ff4f83b0e4941602c2c0ab4f
Meg <shaina.hirschman@gmail.com>
2016-05-23 12:32:09 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/shaina/brachoscounter/BrachosActivity.java
+++ app/src/main/java/com/example/shaina/brachoscounter/BrachosActivity.java
@@ -1,26 +1,47 @@
 package com.example.shaina.brachoscounter;
 
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by Shaina on 5/17/2016.
  */
-public abstract class BrachosActivity extends ListActivity {
+public abstract class BrachosActivity extends AppCompatActivity {
 
-    ListView lview;
-    protected String[] brachos;
+    private BrachosAdapter mBrachosAdapter; // The adapter we used for this ListView
+    private String[] brachos;
+    private ArrayList<String> mListOfCheckedItems; // ArrayList of items to be
+    // passed to the adapter which will add selected items to the list
 
     abstract String[] createArray(); //to force initialization in child classes?
 
     @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putStringArrayList("CHECKED_ITEMS", mListOfCheckedItems);
+        super.onSaveInstanceState(outState);
+    }
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        brachos = createArray();
+
+        setContentView(R.layout.activity_main);
+        setupToolBar();
+        initializeArrays(savedInstanceState);
+        setupListView();
+        setupFAB();
+
+       /* brachos = createArray();
         BrachosAdapter adapter = new BrachosAdapter(this, brachos);
 
         // Assign adapter to ListView
@@ -43,17 +64,79 @@ public abstract class BrachosActivity extends ListActivity {
         });*/
     }
 
-    protected void onListItemClick(ListView l, View v, int position, long id) {
+    private void setupToolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    private void initializeArrays(Bundle savedInstanceState) {
+        // initialize the list to be put into the ListView
+        brachos = new String[25];
+
+        // fill up the list of all items
+        for (int i = 0; i < brachos.length; i++) {
+            brachos[i] = "Item #" + (i + 1);
+        }
+
+        // initialize the list to be passed into the Adapter
+        // to hold the items whose respective buttons are clicked
+
+        // if we have no saved bundle, then make a new list; else, use the ArrayList from bundle
+        mListOfCheckedItems = (savedInstanceState == null ?
+                new ArrayList<String>(18) :
+                savedInstanceState.getStringArrayList("CHECKED_ITEMS"));
+    }
+
+    private void setupListView() {
+        ListView list = (ListView) findViewById(R.id.listView);
+
+        assert mListOfCheckedItems != null;
+        mBrachosAdapter = new BrachosAdapter(this, brachos, R.layout.listview_row,
+                R.id.brachaOption, R.id.addButton,
+                mListOfCheckedItems);
+        list.setAdapter(mBrachosAdapter);
+    }
+
+    private void setupFAB() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Items added: " + mListOfCheckedItems.size()
+                        + "; Total items " + mBrachosAdapter.getCount() + ".", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+    /*protected void onListItemClick(ListView l, View v, int position, long id) {
         String item = (String) getListAdapter().getItem(position);
         Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
-    }
+    }*/
 
 
     //What is this for?
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //   getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/shaina/brachoscounter/BrachosAdapter.java
+++ app/src/main/java/com/example/shaina/brachoscounter/BrachosAdapter.java
@@ -1,38 +1,80 @@
 package com.example.shaina.brachoscounter;
 
-import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by Shaina on 5/16/2016.
  */
 public class BrachosAdapter extends ArrayAdapter<String> {
 
-    private final Activity mCONTEXT;
-    private final String[] mBRACHOS;
+    /* private final Activity mCONTEXT;
+     private final String[] mBRACHOS; */
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+    private int mRowLayoutResourceId, mRowTextViewResourceId, mRowButtonResourceId;
+    private String[] mAdapterData;
+    private ArrayList<String> mClickedItemsList;
 
-    static class ViewHolder {
-        public TextView text;
-        public ImageView image;
+    public BrachosAdapter(Context context, String[] objects, int rowResourceId,
+                          int rowTextViewResourceId, int rowButtonResourceId,
+                          ArrayList<String> clickedItemsList) {
+        // superclass will handle the String array portion of this, like getCount(), etc.
+        super(context, rowResourceId, objects);
+
+        // store a reference to the Context
+        mContext = context;
+
+        // We will need this later to inflate each new row
+        mLayoutInflater = LayoutInflater.from(context);
+
+        // We will need this later to pass to the LayoutInflater to inflate this res. as the new row
+        mRowLayoutResourceId = rowResourceId;
+
+        // We will need these later to know what are the IDs of the resources inside the Row XML
+        mRowTextViewResourceId = rowTextViewResourceId;
+        mRowButtonResourceId = rowButtonResourceId;
+
+        // Store a reference to the list passed in from the Activity/Fragment to add to the list
+        mClickedItemsList = clickedItemsList;
+
+        // Store a reference to the Adapter's data source
+        mAdapterData = objects;
+
+        // The list must have been already initialized or else we can't add any items to it later
+        if (mClickedItemsList == null) {
+            throw new NullPointerException
+                    ("The items list must be initialized before being passed in to the Adapter");
+        }
     }
-
-    public BrachosAdapter(Activity context, String[] values) {
+    /*public BrachosAdapter(Activity context, String[] values) {
         super(context, R.layout.listview_row, values);
         mCONTEXT = context;
         mBRACHOS = values;
+    }*/
+
+    static class ViewHolder {
+        /*public TextView text;
+        public ImageView image;*/
+        TextView mCurrentTextView;
+        Button mCurrentButton;
     }
 
-    @Override
+    /*@Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View rowView = convertView;
         // reuse views
         if (rowView == null) {
-            LayoutInflater inflater = mCONTEXT.getLayoutInflater();
+            LayoutInflater inflater = mContext.getLayoutInflater();
             rowView = inflater.inflate(R.layout.listview_row, null);
             // configure view holder
             ViewHolder viewHolder = new ViewHolder();
@@ -48,6 +90,79 @@ public class BrachosAdapter extends ArrayAdapter<String> {
         holder.text.setText(s);
 
         return rowView;
+    }*/
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        // set the current row to either the converted View or, if null, get a new View
+        View currentRow = convertView != null ? convertView : getNewView(parent);
+
+        // regardless of whether or not we have created a new row or converted one from before
+        final ViewHolder currentViewHolder = (ViewHolder) currentRow.getTag();
+
+        // set the text of the current TextView to whatever the text is in the ArrayAdapter
+        currentViewHolder.mCurrentTextView.setText(getItem(position));
+
+        // Store a reference to the current text
+        final String currentText = currentViewHolder.mCurrentTextView.getText().toString();
+
+        // show/hide the button if the ArrayList does not/does contain this item
+        currentViewHolder.mCurrentButton.setText(
+                mClickedItemsList.contains(currentText) ? "Remove from list" : "Add to list");
+
+        // set the listener
+        currentViewHolder.mCurrentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String actionPerformed;
+
+                if (((Button) v).getText().toString().contains("Remove")) {
+                    actionPerformed = "Removed ";
+
+                    // Remove the current row's text from the list
+                    mClickedItemsList.remove(currentText);
+
+                } else {
+                    actionPerformed = "Added ";
+
+                    // Add the current row's text to the list
+                    mClickedItemsList.add(currentText);
+                }
+
+                // Output that the current text was just added or removed
+                Toast.makeText(mContext, actionPerformed + currentText,
+                        Toast.LENGTH_SHORT).show();
+
+
+                // change the button text since the item was added to or removed from the ArrayList
+                notifyDataSetChanged();
+            }
+        });
+
+        // set the current row as the View to return when getView is called
+        return currentRow;
+    }
+
+    @NonNull
+    private View getNewView(ViewGroup parent) {
+        // inflate a new instance of the XML layout passed in to the constructor
+        View newRow = mLayoutInflater.inflate(mRowLayoutResourceId, parent, false);
+
+        // create a new instance of the custom ViewHolder class above
+        ViewHolder newViewHolder = new ViewHolder();
+
+        // set the TextView and Button member fields in this ViewHolder object to refer to
+        // the newly-inflated current row's TextView and Button (respectively)
+        newViewHolder.mCurrentTextView = (TextView) newRow.findViewById(mRowTextViewResourceId);
+        newViewHolder.mCurrentButton = (Button) newRow.findViewById(mRowButtonResourceId);
+
+        // store a reference to this ViewHolder object in the newly-inflated row's Tag attribute
+        // Now, we won't need to do a findViewById when processing a converted object in getView
+        // because we just stored a reference to each object in the ViewHolder
+        newRow.setTag(newViewHolder);
+
+        // Return the newly-inflated row that also contains a ViewHolder reference in its tag
+        return newRow;
     }
 }
 
