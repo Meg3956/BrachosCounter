cd0887e3332373049723d5755f95d8b129ac2931
mintedtech <mintedtech@gmail.com>
2016-05-24 08:17:20 -0400

    Added Menu  and some event handlers

--- app/src/main/java/com/example/shaina/brachoscounter/MainActivity.java
+++ app/src/main/java/com/example/shaina/brachoscounter/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,11 +17,12 @@ import java.util.ArrayList;
 
 //import com.google.gson.Gson;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     private final static String sPREFS_FIELDS = "PREFS_FIELDS";
-    private final static String sBRACHOS_DESCRIPTION="BRACHOS_DESCRIPTIONS";
-    private final static String sBRACHOS_NUMBERS="BRACHOS_NUMBERS";
+    private final static String sBRACHOS_DESCRIPTION = "BRACHOS_DESCRIPTIONS";
+    private final static String sBRACHOS_NUMBERS = "BRACHOS_NUMBERS";
     final int SINGLE_BRACHOS_REQUEST_CODE = 1;
     final int MULTI_BRACHOS_REQUEST_CODE = 2;
     final int MULTI_BRACHOS_MULTIPLE_REQUEST_CODE = 3;
@@ -29,35 +32,36 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Integer> brachosNumbersToAdd;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        brachosDescriptions = new ArrayList<>();
-        brachosNumbers = new ArrayList<>();
-        brachosDescriptionsToAdd=new ArrayList<>();
-        brachosNumbersToAdd = new ArrayList<>();
-
-
+    protected void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+        brachosDescriptions = new ArrayList<> ();
+        brachosNumbers = new ArrayList<> ();
+        brachosDescriptionsToAdd = new ArrayList<> ();
+        brachosNumbersToAdd = new ArrayList<> ();
     }
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult (int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult (requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
                 case MULTI_BRACHOS_MULTIPLE_REQUEST_CODE:
-                    ArrayList<Integer> numbers = data.getIntegerArrayListExtra("BRACHOS_NUMBERS");
-                    if (!numbers.isEmpty()) {
-                        brachosDescriptionsToAdd.addAll(data.getStringArrayListExtra("BRACHOS_DESCRIPTIONS"));
-                        brachosNumbersToAdd.addAll(numbers);
+                    ArrayList<Integer> numbers = data.getIntegerArrayListExtra ("BRACHOS_NUMBERS");
+                    if (!numbers.isEmpty ()) {
+                        brachosDescriptionsToAdd.addAll (
+                                data.getStringArrayListExtra ("BRACHOS_DESCRIPTIONS"));
+                        brachosNumbersToAdd.addAll (numbers);
                     }
                     break;
                 case MULTI_BRACHOS_REQUEST_CODE:
-                    int number = data.getIntExtra("BRACHOS_NUMBER", 1);
+                    int number = data.getIntExtra ("BRACHOS_NUMBER", 1);
                     if (number > 0) {
-                        brachosDescriptionsToAdd.add(data.getStringExtra("BRACHOS_DESCRIPTION"));
-                        brachosNumbersToAdd.add(number);
+                        brachosDescriptionsToAdd.add (data.getStringExtra ("BRACHOS_DESCRIPTION"));
+                        brachosNumbersToAdd.add (number);
                     }
                     break;
                 case SINGLE_BRACHOS_REQUEST_CODE:
@@ -67,48 +71,55 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void launchDaveningPage(View view) {
-        Intent intent = new Intent(this, DaveningActivity.class);
-        startActivityForResult(intent, MULTI_BRACHOS_MULTIPLE_REQUEST_CODE);
+    public void launchDaveningPage (View view)
+    {
+        Intent intent = new Intent (this, DaveningActivity.class);
+        startActivityForResult (intent, MULTI_BRACHOS_MULTIPLE_REQUEST_CODE);
     }
 
-    public void launchAddYourOwnPage(View view) {
-        Intent intent = new Intent(this, AddYourOwnActivity.class);
-        startActivityForResult(intent, MULTI_BRACHOS_REQUEST_CODE);
+    public void launchAddYourOwnPage (View view)
+    {
+        Intent intent = new Intent (this, AddYourOwnActivity.class);
+        startActivityForResult (intent, MULTI_BRACHOS_REQUEST_CODE);
     }
 
-    public void launchFoodDrink(View view) {
-        Intent intent = new Intent(this, FoodDrinkActivity.class);
+    public void launchFoodDrink (View view)
+    {
+        Intent intent = new Intent (this, BrachosActivity.class);
         String[] foodBrachos = {"Hamotzi", "Mezonos", "Hagafen", "Haetz", "Ha'adama", "Shehakol",
-                "Birkas Hamazon", "Al Hamichya", "Borei Nefashos"};
-        intent.putExtra(getString(R.string.brachosList), foodBrachos);
-        startActivityForResult(intent, MULTI_BRACHOS_REQUEST_CODE);
+                                "Birkas Hamazon", "Al Hamichya", "Borei Nefashos"};
+        intent.putExtra (getString (R.string.brachosList), foodBrachos);
+        startActivityForResult (intent, MULTI_BRACHOS_REQUEST_CODE);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-       // restorePreferencesSavedFromSettingsActivity();
-        restoreNonSettingsActivityPreferences();
-        addBrachosFromRestoredActivity();
+    protected void onStart ()
+    {
+        super.onStart ();
+        // restorePreferencesSavedFromSettingsActivity();
+        restoreNonSettingsActivityPreferences ();
+        addBrachosFromRestoredActivity ();
 
     }
 
     //TODO: Do we need an onResume to restore things?
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onResume ()
+    {
+        super.onResume ();
        /* applyNightModePreference();
         showHideBackground ();*/
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        saveNonSettingsActivityPreferences();
+    protected void onStop ()
+    {
+        super.onStop ();
+        saveNonSettingsActivityPreferences ();
     }
 
-    private void restorePreferencesSavedFromSettingsActivity() {
+    private void restorePreferencesSavedFromSettingsActivity ()
+    {
         String currentKey;
         String currentDefaultValue;
 
@@ -116,8 +127,8 @@ public class MainActivity extends AppCompatActivity {
         // Used for persisting state to storage
 
         // First, get handle to user settings/preferences
-     //   SharedPreferences defaultSharedPreferences =
-           //     PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        //   SharedPreferences defaultSharedPreferences =
+        //     PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
         //TODO: Set to our preferences!!!
         // Show Background Picture Preference
@@ -126,17 +137,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void restoreNonSettingsActivityPreferences() {
+    private void restoreNonSettingsActivityPreferences ()
+    {
         //TODO: Set to our lists.
 
 
         SharedPreferences settings = getSharedPreferences (sPREFS_FIELDS, MODE_PRIVATE);
-        String descriptionString=settings.getString(sBRACHOS_DESCRIPTION,"");
-        if (!descriptionString.isEmpty()){
-            brachosDescriptions=(ArrayList<String>)restoreListFromJSON(descriptionString);
+        String descriptionString = settings.getString (sBRACHOS_DESCRIPTION, "");
+        if (!descriptionString.isEmpty ()) {
+            brachosDescriptions = (ArrayList<String>) restoreListFromJSON (descriptionString);
 
-            brachosNumbers=(ArrayList<Integer>)restoreListFromJSON(settings.getString(sBRACHOS_NUMBERS, "[]"));
-
+            brachosNumbers = (ArrayList<Integer>) restoreListFromJSON (
+                    settings.getString (sBRACHOS_NUMBERS, "[]"));
 
 
         }
@@ -162,16 +174,17 @@ public class MainActivity extends AppCompatActivity {
 */
     }
 
-    private void saveNonSettingsActivityPreferences() {
-        SharedPreferences settings = getSharedPreferences(sPREFS_FIELDS, MODE_PRIVATE); //MP==0
-        SharedPreferences.Editor settingsEditor = settings.edit();
+    private void saveNonSettingsActivityPreferences ()
+    {
+        SharedPreferences settings = getSharedPreferences (sPREFS_FIELDS, MODE_PRIVATE); //MP==0
+        SharedPreferences.Editor settingsEditor = settings.edit ();
 
-        settingsEditor.clear();
+        settingsEditor.clear ();
 
-        String jsonBrachosDescriptions = getJSON(brachosDescriptions);
-        String jsonBrachosNumbers=getJSON(brachosNumbers);
-        settingsEditor.putString(sBRACHOS_DESCRIPTION,jsonBrachosDescriptions);
-        settingsEditor.putString(sBRACHOS_NUMBERS,jsonBrachosNumbers);
+        String jsonBrachosDescriptions = getJSON (brachosDescriptions);
+        String jsonBrachosNumbers = getJSON (brachosNumbers);
+        settingsEditor.putString (sBRACHOS_DESCRIPTION, jsonBrachosDescriptions);
+        settingsEditor.putString (sBRACHOS_NUMBERS, jsonBrachosNumbers);
         // Tax and tip are derived from values stored automatically via Settings Activity
         // So we need to store only the other two EditTexts
         //TODO: add our prefs
@@ -180,51 +193,61 @@ public class MainActivity extends AppCompatActivity {
         // settingsEditor.putString (mSUBTOTAL_PREF_KEY, mSubTotalField.getText ().toString ());
         //settingsEditor.putString (mPAYERS_PREF_KEY, mPayersField.getText ().toString ());
 
-        settingsEditor.apply();
+        settingsEditor.apply ();
     }
 
-    private String getJSON(ArrayList obj) {
-        Gson gson = new Gson();
-        String json = gson.toJson(obj);
+    private String getJSON (ArrayList obj)
+    {
+        Gson gson = new Gson ();
+        String json = gson.toJson (obj);
 
 
         return json;
     }
 
-    private ArrayList restoreListFromJSON(String json){
-        Gson gson = new Gson();
-        ArrayList obj = gson.fromJson(json, ArrayList.class);
+    private ArrayList restoreListFromJSON (String json)
+    {
+        Gson gson = new Gson ();
+        ArrayList obj = gson.fromJson (json, ArrayList.class);
         return obj;
     }
 
-    private void addBrachosFromRestoredActivity(){
-        brachosDescriptions.addAll(brachosDescriptionsToAdd);
-        brachosDescriptionsToAdd.clear();
+    private void addBrachosFromRestoredActivity ()
+    {
+        brachosDescriptions.addAll (brachosDescriptionsToAdd);
+        brachosDescriptionsToAdd.clear ();
 
-       brachosNumbers.addAll(brachosNumbersToAdd);
-        brachosNumbersToAdd.clear();
+        brachosNumbers.addAll (brachosNumbersToAdd);
+        brachosNumbersToAdd.clear ();
     }
 
-    public void viewTotalBrachos(View view) {
-        int counter=0;
+    public void viewTotalBrachos (View view)
+    {
+        viewTotalBrachos ();
+    }
+
+    private void viewTotalBrachos ()
+    {
+        int counter = 0;
         for (Integer brachosNumber : brachosNumbers) {
-            counter+=brachosNumber;
+            counter += brachosNumber;
         }
       /*  Toast.makeText(
                 getApplicationContext(),
                 brachosNumbers.toString() + "", Toast.LENGTH_SHORT)
                 .show();*/
-        Toast.makeText(
-                getApplicationContext(),
+        Toast.makeText (
+                getApplicationContext (),
                 counter + "", Toast.LENGTH_SHORT)
-                .show();
+                .show ();
     }
 
-    public void clearBrachos(View view) {
+    public void clearBrachos (View view)
+    {
         //call addBrachosFromRestoredActivity to flush the 'ToAdd' ArrayLists if click before they are flushed
-        addBrachosFromRestoredActivity();
-        brachosDescriptions.clear();
-        brachosNumbers.clear();
+        addBrachosFromRestoredActivity ();
+        brachosDescriptions.clear ();
+        brachosNumbers.clear ();
     }
 
 
@@ -236,6 +259,32 @@ public class MainActivity extends AppCompatActivity {
         set(key, json);
     }*/
 
+    @Override public boolean onCreateOptionsMenu (Menu menu)
+    {
+        getMenuInflater ().inflate (R.menu.menu_main, menu);
+        return true; //super.onCreateOptionsMenu (menu);
+    }
+
+    @Override public boolean onOptionsItemSelected (MenuItem item)
+    {
+        int id = item.getItemId ();
+
+        //noinspection SimplifiableIfStatement
+        switch (id)
+        {
+            case R.id.action_settings: {
+                //showSettingsActivity ();
+                return true;
+            }
+            case R.id.action_view_total: {
+                viewTotalBrachos();
+                return true;
+            }
+
+        }
+
+        return super.onOptionsItemSelected (item);
+    }
 
     // LG work-around
     @Override
